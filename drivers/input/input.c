8fb4c6abb836485b75c0e711f4ab8c8f44f2ba8e
Stratos Karafotis <stratosk@semaphore.gr>
2013-08-30 20:33:06 +0300

    Revert "touchwake: Handle touch suspend/resume only if touchwake is enabled"
    This reverts commit 2bc75c0a586113935fb28c621ff05c0f1254ee4d.

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -304,8 +304,7 @@ static int input_get_disposition(struct input_dev *dev,
 		}
 =======
 #ifdef CONFIG_TOUCH_WAKE
-			if (code == KEY_POWER && touchwake_is_enabled() &&
-						!device_is_suspended()) {
+			if (code == KEY_POWER && !device_is_suspended()) {
 				if (value == 1)
 					powerkey_pressed();
 				else if (value == 0)
