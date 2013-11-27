4b1d118aba60e11b45401c0b61e5fe12eefce2d2
Paul Beeler <sparksco@gmail.com>
2013-11-27 00:58:12 -0500

    Zulfa CM11 v1
    Signed-off-by: Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>

--- bina-stok.sh
+++ bina-stok.sh
@@ -9,7 +9,7 @@ export CROSS_COMPILE=/opt/sabermod-gcc/bin/arm-eabi-
 
 export KBUILD_BUILD_USER=najmi
 export KBUILD_BUILD_HOST="ampang"
-export LOCALVERSION="-zulfa-cm11-v1"
+#export LOCALVERSION="-zulfa-cm11-v1"
 
 #echo "Copy backup config..."
 #cp najmi-mako-config .config
