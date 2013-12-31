f59334c3ccad5789203ed8b092f155e2b3d47b4d
raden <najmi.zabidi@gmail.com>
2013-12-31 16:12:40 +0800

    update compile file

--- bina-stok.sh
+++ bina-stok.sh
@@ -4,9 +4,9 @@ unset CROSS_COMPILE
 #export CROSS_COMPILE=/opt/gcc-4.8-linaro/bin/arm-cortex_a15-linux-gnueabi- 
 #export CROSS_COMPILE=/opt/gcc-4.7-linaro/bin/arm-cortex_a15-linux-gnueabi- 
 
-export CROSS_COMPILE=/opt/AK-linaro/4.7.3-2013.04.20130415/bin/arm-linux-gnueabihf-
+#export CROSS_COMPILE=/opt/AK-linaro/4.7.3-2013.04.20130415/bin/arm-linux-gnueabihf-
 #export CROSS_COMPILE=/opt/AK-linaro/4.8.3-2013.11.20131205/bin/arm-linux-gnueabihf-
-#export CROSS_COMPILE=/opt/sabermod-gcc/bin/arm-eabi-
+export CROSS_COMPILE=/opt/sabermod-gcc/bin/arm-eabi-
 
 #echo "Cleaning old craps..."
 #make distclean
