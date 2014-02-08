bbe0b2821e8ed938454ff507c51a66d2fc5933e4
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-02-08 22:14:12 +0800

    Kconfig and acpuclock-krait alteration for underclock

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -50,6 +50,12 @@
 static DEFINE_MUTEX(driver_lock);
 static DEFINE_SPINLOCK(l2_lock);
 
+#ifdef CONFIG_LOW_CPUCLOCKS
+#define FREQ_TABLE_SIZE    39
+#else
+#define FREQ_TABLE_SIZE    35
+#endif
+
 #ifdef CONFIG_DEBUG_FS
 static int gpvs_bin;
 #endif
@@ -914,7 +920,8 @@ static void __init bus_init(const struct l2_level *l2_level)
 }
 
 #ifdef CONFIG_CPU_FREQ_MSM
-static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
+//static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
+static struct cpufreq_frequency_table freq_table[NR_CPUS][FREQ_TABLE_SIZE];
 
 static void __init cpufreq_table_init(void)
 {
