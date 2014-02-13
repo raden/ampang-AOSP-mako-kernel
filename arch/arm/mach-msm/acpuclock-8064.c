dc6b641efbad7372efdf96f21bed99466c3b425b
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-02-13 22:51:03 +0800

    msm: acpuclock-8064: Overclock version, using @faux123 krait CONFIG definition
    Signed-off-by: Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>

--- arch/arm/mach-msm/acpuclock-8064.c
+++ arch/arm/mach-msm/acpuclock-8064.c
@@ -46,7 +46,12 @@ static struct scalable scalable[] __initdata = {
 		.aux_clk_sel = 3,
 		.sec_clk_sel = 2,
 		.l2cpmr_iaddr = 0x4501,
+
+#ifdef CONFIG_CPUCLOCK_TINGGI
+		.vreg[VREG_CORE] = { "krait0", 1450000 },
+#else
 		.vreg[VREG_CORE] = { "krait0", 1300000 },
+#endif
 		.vreg[VREG_MEM]  = { "krait0_mem", 1150000 },
 		.vreg[VREG_DIG]  = { "krait0_dig", 1150000 },
 		.vreg[VREG_HFPLL_A] = { "krait0_hfpll", 1800000 },
@@ -57,7 +62,11 @@ static struct scalable scalable[] __initdata = {
 		.aux_clk_sel = 3,
 		.sec_clk_sel = 2,
 		.l2cpmr_iaddr = 0x5501,
+#ifdef CONFIG_CPUCLOCK_TINGGI
+		.vreg[VREG_CORE] = { "krait1", 1450000 },
+#else
 		.vreg[VREG_CORE] = { "krait1", 1300000 },
+#endif
 		.vreg[VREG_MEM]  = { "krait1_mem", 1150000 },
 		.vreg[VREG_DIG]  = { "krait1_dig", 1150000 },
 		.vreg[VREG_HFPLL_A] = { "krait1_hfpll", 1800000 },
@@ -68,7 +77,11 @@ static struct scalable scalable[] __initdata = {
 		.aux_clk_sel = 3,
 		.sec_clk_sel = 2,
 		.l2cpmr_iaddr = 0x6501,
+#ifdef CONFIG_CPUCLOCK_TINGGI
+		.vreg[VREG_CORE] = { "krait2", 1450000 },
+#else
 		.vreg[VREG_CORE] = { "krait2", 1300000 },
+#endif
 		.vreg[VREG_MEM]  = { "krait2_mem", 1150000 },
 		.vreg[VREG_DIG]  = { "krait2_dig", 1150000 },
 		.vreg[VREG_HFPLL_A] = { "krait2_hfpll", 1800000 },
@@ -79,7 +92,11 @@ static struct scalable scalable[] __initdata = {
 		.aux_clk_sel = 3,
 		.sec_clk_sel = 2,
 		.l2cpmr_iaddr = 0x7501,
+#ifdef CONFIG_CPUCLOCK_TINGGI
+		.vreg[VREG_CORE] = { "krait3", 1450000 },
+#else
 		.vreg[VREG_CORE] = { "krait3", 1300000 },
+#endif
 		.vreg[VREG_MEM]  = { "krait3_mem", 1150000 },
 		.vreg[VREG_DIG]  = { "krait3_dig", 1150000 },
 		.vreg[VREG_HFPLL_A] = { "krait3_hfpll", 1800000 },
@@ -93,6 +110,7 @@ static struct scalable scalable[] __initdata = {
 		.vreg[VREG_HFPLL_A] = { "l2_hfpll", 1800000 },
 	},
 };
+
 
 /*
  * The correct maximum rate for 8064ab in 600 MHZ.
@@ -210,9 +228,9 @@ static struct acpu_level tbl_slow[] __initdata = {
 #ifdef CONFIG_CPUCLOCK_TINGGI
 	{ 1, {  1458000, HFPLL, 1, 0x36 }, L2(14), 1137500 },
 	{ 0, {  1512000, HFPLL, 1, 0x38 }, L2(14), 1150000 },
-	{ 1, {  1674000, HFPLL, 1, 0x3E }, L2(14), 1150000 },
-	{ 1, {  1890000, HFPLL, 1, 0x46 }, L2(14), 1150000 },
-	{ 1, {  1971000, HFPLL, 1, 0x49 }, L2(14), 1150000 },
+	{ 1, {  1674000, HFPLL, 1, 0x3E }, L2(14), 1350000 },
+	{ 1, {  1890000, HFPLL, 1, 0x46 }, L2(14), 1350000 },
+	{ 1, {  1971000, HFPLL, 1, 0x49 }, L2(14), 1450000 },
 #endif
 	{ 0, { 0 } }
 };
@@ -252,9 +270,9 @@ static struct acpu_level tbl_nom[] __initdata = {
 #ifdef CONFIG_CPUCLOCK_TINGGI
 	{ 1, {  1458000, HFPLL, 1, 0x36 }, L2(14), 1137500 },
 	{ 0, {  1512000, HFPLL, 1, 0x38 }, L2(14), 1150000 },
-	{ 1, {  1674000, HFPLL, 1, 0x3E }, L2(14), 1150000 },
-	{ 1, {  1890000, HFPLL, 1, 0x46 }, L2(14), 1150000 },
-	{ 1, {  1971000, HFPLL, 1, 0x49 }, L2(14), 1150000 },
+	{ 1, {  1674000, HFPLL, 1, 0x3E }, L2(14), 1350000 },
+	{ 1, {  1890000, HFPLL, 1, 0x46 }, L2(14), 1350000 },
+	{ 1, {  1971000, HFPLL, 1, 0x49 }, L2(14), 1450000 },
 #endif
 
 	{ 0, { 0 } }
@@ -295,9 +313,9 @@ static struct acpu_level tbl_fast[] __initdata = {
 #ifdef CONFIG_CPUCLOCK_TINGGI
 	{ 1, {  1458000, HFPLL, 1, 0x36 }, L2(14), 1137500 },
 	{ 0, {  1512000, HFPLL, 1, 0x38 }, L2(14), 1150000 },
-	{ 1, {  1674000, HFPLL, 1, 0x3E }, L2(14), 1150000 },
-	{ 1, {  1890000, HFPLL, 1, 0x46 }, L2(14), 1150000 },
-	{ 1, {  1971000, HFPLL, 1, 0x49 }, L2(14), 1150000 },
+	{ 1, {  1674000, HFPLL, 1, 0x3E }, L2(14), 1350000 },
+	{ 1, {  1890000, HFPLL, 1, 0x46 }, L2(14), 1350000 },
+	{ 1, {  1971000, HFPLL, 1, 0x49 }, L2(14), 1450000 },
 #endif
 
 	{ 0, { 0 } }
@@ -338,9 +356,9 @@ static struct acpu_level tbl_faster[] __initdata = {
 #ifdef CONFIG_CPUCLOCK_TINGGI
 	{ 1, {  1458000, HFPLL, 1, 0x36 }, L2(14), 1137500 },
 	{ 0, {  1512000, HFPLL, 1, 0x38 }, L2(14), 1150000 },
-	{ 1, {  1674000, HFPLL, 1, 0x3E }, L2(14), 1150000 },
-	{ 1, {  1890000, HFPLL, 1, 0x46 }, L2(14), 1150000 },
-	{ 1, {  1971000, HFPLL, 1, 0x49 }, L2(14), 1150000 },
+	{ 1, {  1674000, HFPLL, 1, 0x3E }, L2(14), 1350000 },
+	{ 1, {  1890000, HFPLL, 1, 0x46 }, L2(14), 1350000 },
+	{ 1, {  1971000, HFPLL, 1, 0x49 }, L2(14), 1450000 },
 #endif
 
 	{ 0, { 0 } }
