b9c9e19c02f70298655abae3054dd9bcf34fb037
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-02-14 11:44:59 +0800

    Ampang v15 Overclocked
    Signed-off-by: Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>

--- bina-stok.sh
+++ bina-stok.sh
@@ -13,7 +13,7 @@ export CROSS_COMPILE=/opt/sabermod-gcc/bin/arm-eabi-
 
 export KBUILD_BUILD_USER=najmi
 export KBUILD_BUILD_HOST="ampang"
-export LOCALVERSION="-Ampang-AOSP-kernel-v15"
+export LOCALVERSION="-Ampang-AOSP-kernel-v15-overclocked"
 
 #echo "Copy backup config..."
 #cp najmi-mako-config .config
