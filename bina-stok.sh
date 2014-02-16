168d304f362828cc9092e6e1c72924a03f50d1d9
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-02-16 14:24:21 +0800

    Aufa-hc v3
    Signed-off-by: Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>

--- bina-stok.sh
+++ bina-stok.sh
@@ -9,7 +9,7 @@ export CROSS_COMPILE=/opt/sabermod-gcc/bin/arm-eabi-
 
 export KBUILD_BUILD_USER=najmi
 export KBUILD_BUILD_HOST="ampang"
-export LOCALVERSION="-aufa-hc-v2"
+export LOCALVERSION="-aufa-hc-v3"
 
 #echo "Copy backup config..."
 #cp najmi-mako-config .config
