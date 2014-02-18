03303f9ed4eb7ab09458b60aa3f4967c57a44add
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-02-18 10:43:32 +0800

    Revert "cpufreq: ondemand: Fix the boostpulse interface (again)"
    This reverts commit 425bbd8b5eef0f1fbfbd5ea8c2fb26d45f73b2bc.
    
    Conflicts:
    
    	drivers/cpufreq/cpufreq_ondemand.c

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -899,6 +899,14 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 		}
 	}
 
+	/* check for frequency boost */
+	if ((dbs_tuners_ins.boosted || lge_boosted)
+	    && policy->cur < dbs_tuners_ins.boostfreq) {
+		dbs_freq_increase(policy, dbs_tuners_ins.boostfreq);
+		dbs_tuners_ins.boostfreq = policy->cur;
+		return;
+	}
+
 	/* Check for frequency decrease */
 	/* if we cannot reduce the frequency anymore, break out early */
 	if (policy->cur == policy->min)
