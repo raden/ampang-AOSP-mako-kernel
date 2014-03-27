1afc65722807acf73620d8f4e10fe3b1abc1e081
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-03-27 11:19:59 +0800

    Zulfa AOSP v4
    Signed-off-by: Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>

--- bina-stok.sh
+++ bina-stok.sh
@@ -9,7 +9,7 @@ export CROSS_COMPILE=/opt/sabermod-gcc/bin/arm-eabi-
 
 export KBUILD_BUILD_USER=najmi
 export KBUILD_BUILD_HOST="ampang"
-#export LOCALVERSION="-zulfa-AOSP-v2"
+#export LOCALVERSION="-zulfa-AOSP-v4"
 
 #echo "Copy backup config..."
 #cp najmi-mako-config .config
