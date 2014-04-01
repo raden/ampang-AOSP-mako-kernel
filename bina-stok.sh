2a706a043d660fcd5ec2b60bb6a468a9d4b2c445
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-04-01 20:10:26 +0800

    Update build file
    Signed-off-by: Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>

--- bina-stok.sh
+++ bina-stok.sh
@@ -6,14 +6,15 @@ unset CROSS_COMPILE
 
 #export CROSS_COMPILE=/opt/AK-linaro/4.7.3-2013.04.20130415/bin/arm-linux-gnueabihf-
 #export CROSS_COMPILE=/opt/AK-linaro/4.8.3-2013.11.20131205/bin/arm-linux-gnueabihf-
-export CROSS_COMPILE=/opt/sabermod-gcc/bin/arm-eabi-
+#export CROSS_COMPILE=/opt/sabermod-gcc/bin/arm-eabi-
+export CROSS_COMPILE=/opt/gcc-linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.8.3-2014.02/bin/arm-cortex_a15-linux-gnueabihf-
 
 #echo "Cleaning old craps..."
 #make distclean
 
 export KBUILD_BUILD_USER=najmi
 export KBUILD_BUILD_HOST="ampang"
-export LOCALVERSION="-Ampang-AOSP-v22"
+export LOCALVERSION="-Ampang-AOSP-v24"
 
 #echo "Copy backup config..."
 #cp najmi-mako-config .config
