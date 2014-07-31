88fbcbdbd31f65974e9ba8c377c6c9c7c7075fef
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-08-01 00:51:09 +0800

    update build file
    Signed-off-by: Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>

--- bina-stok.sh
+++ bina-stok.sh
@@ -7,8 +7,7 @@ unset CROSS_COMPILE
 #export CROSS_COMPILE=/opt/AK-linaro/4.7.3-2013.04.20130415/bin/arm-linux-gnueabihf-
 #export CROSS_COMPILE=/opt/AK-linaro/4.8.3-2013.11.20131205/bin/arm-linux-gnueabihf-
 #export CROSS_COMPILE=/opt/sabermod-gcc/bin/arm-eabi-
-export CROSS_COMPILE=/opt/gcc-linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.8.3-2014.02/bin/arm-cortex_a15-linux-gnueabihf-
-
+export CROSS_COMPILE=/opt/gcc-linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.07/bin/arm-cortex_a15-linux-gnueabihf-
 #echo "Cleaning old craps..."
 #make distclean
 
